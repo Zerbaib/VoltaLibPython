d9f3fd545caf875c45c49ab52c2055c3dedf9b0c
Hugo Hennetin <hugo.hennetin@proton.me>
2026-08-30 03:40:06 +0200

    Refactor VoltaClient methods and update README with usage examples for library requests

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ def test_timing_refresh():
             time.sleep(1)
         print(" " * 40, end="\r", flush=True)
 
-def test_fonction_client():
-    client = VoltaClient()
-    print(client.POST().Library().tracks({"track_id": ""}))
+def main():
+    with VoltaClient() as client:
+        tracks = client.get.library.tracks()
+        albums = client.get.library.albums()
+        artists = client.get.library.artists()
+        artists_albums = client.get.library.artist_albums("artist_id")
+        artists_tracks = client.get.library.artist_tracks("artist_id")
+        playlists = client.get.library.playlists()
+        playlists_id = client.get.library.playlists("playlist_id")
 
-test_fonction_client()
+main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="VoltaLib",
-	version="0.2.1",
+	version="0.3.0",
 	description="VoltaLibPython packaged as VoltaLib",
 	packages=find_packages(exclude=("tests", "docs")),
 	include_package_data=True,
