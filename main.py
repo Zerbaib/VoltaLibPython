620c157b430ce237ed6d690f982c0cc7e58aef42
Hugo Hennetin <hugo.hennetin@proton.me>
2026-08-30 11:14:46 +0200

    Update version to 0.3.3 and refactor client and main module for improved functionality and readability

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import json
+import os
 
 from VoltaLibPython import VoltaClient
 
 def main():
     with VoltaClient() as client:
-        #tracks = client.get.library.tracks()
-        #albums = client.get.library.albums()
-        #artists = client.get.library.artists()
-        #artists_albums = client.get.library.artist_albums("m")
-        #artists_tracks = client.get.library.artist_tracks("m")
-        #playlists = client.get.library.playlists()
-        #playlists_tracks = client.get.library.playlist_tracks("3d4aefb7-0a8b-43ac-9f91-6fc4f1dbb62f")
-        
-        client.post.track({
-            "id": "11111111-1111-1111-1111-111111111111",
-            "name": "Test Track",
-            "artist": "Test Artist",
-            "album": "Test Album",
-            "duration": 180,
-        })
+        client.get.library.tracks()
 
-        pass
+        with open("tracks.json", "w") as f:
+            json.dump(client.get.library.tracks(), f, indent=4)
+
+def pause():
+    input("Press Enter to exit and clear...")
+    clear()
+
+def clear():
+    for filename in os.listdir("."):
+        if filename.endswith(".json"):
+            os.remove(filename)
 
 main()
+pause()
